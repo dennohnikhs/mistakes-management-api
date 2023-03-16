e4c96b30ffcf965b69c7148b5d018d3703570123
Langat Denis <davidsonlangat2021@gmail.com>
2023-03-16 16:02:56 +0300

    enabled http requests to the database

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const http = require("http");
 
+dotenv.config({ path: ".env" });
 const app = express();
 app.use(cors());
-dotenv.config({ path: ".env" });
 const v1Router = require("./src/v1/routes/index");
 const swaggerDocs = require("./swagger");
 const authMiddleware = require("./src/middleware/auth-middleware");
@@ -14,12 +15,13 @@ app.get("/", (req, res) => {
 });
 app.use(express.json());
 app.use(cookieParser());
+const server = http.createServer(app);
 app.use(express.urlencoded({ extended: true }));
 app.use(authMiddleware);
 app.use(v1Router);
 const port = process.env.PORT || 5500;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
   swaggerDocs(app, port);
 });
